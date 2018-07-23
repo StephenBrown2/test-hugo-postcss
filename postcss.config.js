8ea1547de67e11bdbc9b148bbcd2fe40a4aaab43
Stephen Brown II <stephen.brownii@rackspace.com>
2018-07-23 15:27:41 -0500

    After installing node modules locally - not working

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "plugins": {
+        "postcss-import": {},
+        "postcss-grid-kiss": {
+            "fallback": true
+        },
+        "postcss-preset-env": {
+            "browsers": ["> 1%", "last 2 versions"]
+        }
+    }
+}
